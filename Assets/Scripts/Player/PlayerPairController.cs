8ffebe87a6b7a137c9275c13e2bb8ef6839a65dc
dennis <55745095+twistedPan@users.noreply.github.com>
2023-02-04 17:17:59 +0100

    only two player can spawn

--- Assets/Scripts/Player/PlayerPairController.cs
+++ Assets/Scripts/Player/PlayerPairController.cs
@@ -19,23 +19,53 @@ public class PlayerPairController : MonoBehaviour
         managedPlayers = new Player[2];
     }
 
-    public void OnMovePlayer1(InputValue value) => managedPlayers[0]?.SetPlayerMoveInput(value.Get<Vector2>());
-    
-    public void OnMovePlayer2(InputValue value) => managedPlayers[1]?.SetPlayerMoveInput(value.Get<Vector2>());
+    public void OnMovePlayer1(InputValue value)
+    {
+        if (managedPlayers[0] == null) return;
+        managedPlayers[0].SetPlayerMoveInput(value.Get<Vector2>());
+    }
 
-    public void OnStopPlayer1() => managedPlayers[0]?.SetPlayerMoveInput(Vector2.zero);
-    
-    public void OnStopPlayer2() => managedPlayers[1]?.SetPlayerMoveInput(Vector2.zero);
+    public void OnMovePlayer2(InputValue value)
+    {
+        if (managedPlayers[1] == null) return;
+        managedPlayers[1].SetPlayerMoveInput(value.Get<Vector2>());
+    }
 
-    public void OnActionPlayer1() => managedPlayers[0]?.Action();
+    public void OnStopPlayer1()
+    {
+        if (managedPlayers[0] == null) return;
+        managedPlayers[0].SetPlayerMoveInput(Vector2.zero);
+    }
 
-    public void OnActionPlayer2() => managedPlayers[1]?.Action();
+    public void OnStopPlayer2()
+    {
+        if (managedPlayers[1] == null) return;
+        managedPlayers[1].SetPlayerMoveInput(Vector2.zero);
+    }
+
+    public void OnActionPlayer1()
+    {
+        if (managedPlayers[0] == null) return;
+        managedPlayers[0].Action();
+    }
+
+    public void OnActionPlayer2()
+    {
+        if (managedPlayers[1] == null) return;
+        managedPlayers[1].Action();
+    }
 
     public void OnJoinPlayer1(InputValue value)
     {
-        Debug.Log("Player 1 joined " + value);
-        Vector2 spawnPosition = Random.insideUnitCircle * 50;
-        Vector3 spawnPositionXZ = new Vector3(spawnPosition.x, 0, spawnPosition.y);
+        if (managedPlayers[0] != null)
+        {
+            /*RemovePlayer(managedPlayers[0]);
+            managedPlayers[0] = null;*/
+            return;
+        }
+        Debug.Log("Player 1 joined!");
+        Vector2 spawnPosition = new Vector3(30 + (5 * Random.value), 0, 30 + (5 * Random.value));
+        Vector3 spawnPositionXZ = new Vector3(spawnPosition.x, 1, spawnPosition.y);
         GameObject newPlayer = Instantiate(playerPrefab, spawnPositionXZ, Quaternion.identity);
 
         managedPlayers[0] = newPlayer.GetComponent<Player>();
@@ -44,12 +74,24 @@ public class PlayerPairController : MonoBehaviour
 
     public void OnJoinPlayer2()
     {
-        Debug.Log("Player 2 joined");
-        Vector2 spawnPosition = Random.insideUnitCircle * 50;
-        Vector3 spawnPositionXZ = new Vector3(spawnPosition.x, 0, spawnPosition.y);
+        if (managedPlayers[1] != null)
+        {
+            /*RemovePlayer(managedPlayers[1]);
+            managedPlayers[1] = null;*/
+            return;
+        }
+        Debug.Log("Player 2 joined!");
+        Vector2 spawnPosition = new Vector3(30 + (5 * Random.value), 0, 30 + (5 * Random.value));
+        Vector3 spawnPositionXZ = new Vector3(spawnPosition.x, 1, spawnPosition.y);
         GameObject newPlayer = Instantiate(playerPrefab, spawnPositionXZ, Quaternion.identity);
 
         managedPlayers[1] = newPlayer.GetComponent<Player>();
         cameraScript.AddPlayerToCamera("noId" + (int)(Random.value * 5), newPlayer.transform);
+    }
+
+    private void RemovePlayer(Player player)
+    {
+        cameraScript.RemovePlayerFromCamera("no id");
+        Destroy(player.gameObject);
     }
 }
